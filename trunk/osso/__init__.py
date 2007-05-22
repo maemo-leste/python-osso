0ce63442a3a3f9c1ba5cbc7450c0336c8f9a910e
lucianomw <lucianomw@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-05-22 13:02:09 +0000

    -including osso-helplib
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@292 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/osso/__init__.py
+++ trunk/osso/__init__.py
@@ -25,6 +25,7 @@ from context import Context
 from application import Application
 from autosave import Autosave
 from device_state import DeviceState
+from help import Help
 from mime import Mime
 from plugin import Plugin
 from rpc import Rpc
