6f55c78165d3fef571a95ab920970db61596a415
lmoura <lmoura@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-05-29 21:09:35 +0000

    Implemented callbacks
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@314 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/examples/example_ic.py
+++ trunk/examples/example_ic.py
@@ -1,3 +1,7 @@
+# Simple example of using osso.ic module
+# Author: Lauro Moura <lauro.neto@indt.org.br>
+
+
 import osso.ic
 import gnome.gconf
 import gobject 
@@ -8,6 +12,8 @@ class ExampleIc(hildon.Program):
     def __init__(self):
         hildon.Program.__init__(self)
 
+        self.stat_handle = None
+
         self.window = hildon.Window()
         self.window.connect("destroy", self._exit)
 
@@ -17,8 +23,6 @@ class ExampleIc(hildon.Program):
 
         self.buttons.pack_start(self.bt_connect)
         self.buttons.pack_start(self.bt_disconnect)
-
-        self.bt_disconnect.set_sensitive(False)
 
         self.bt_connect.connect("clicked", self._bt_connect_cb)
         self.bt_disconnect.connect("clicked", self._bt_disconnect_cb)
@@ -81,25 +85,66 @@ class ExampleIc(hildon.Program):
         gtk.main()
 
     def _ic_cb(self, evt, *args):
-        if evt.type == osso.ic.IAP_CONNECTED:
-            self.bt_connect.set_sensitive(False)
-            self.bt_disconnect.set_sensitive(True)
-        elif evt.type == osso.ic.IAP_DISCONNECTED:
-            self.bt.disconnect.set_sensitive(False)
-            self.bt_connect.set_sensitive(True)
-        elif evt.type == osso.ic.IAP_STATISTICS:
+
+        print "_ic_cb()"
+        print "evt = ", evt
+        print args
+
+        if evt.type == osso.ic.CONNECTED:
+            self.status.set_text("Connected")
+
+            if self.stat_handle == None:
+                self.stat_handle = gobject.timeout_add(1000, self._req_stats)
+
+        elif evt.type == osso.ic.DISCONNECTED:
+            self.status.set_text("Disconnected")
+
+            if self.stat_handle != None:
+                gobject.source_remove(self.stat_handle)
+                self.stat_handle = None
+
+        elif evt.type == osso.ic.STATISTICS:
+            print "Statistics event"
             self._update_stats(evt)
-        elif evt.type == osso.ic.IAP_ERROR:
-            pass
+
+        elif evt.type == osso.ic.ERROR:
+            print "Error connecting"
+            print evt.error_code
 
     def _bt_connect_cb(self, bt):
-        osso.ic.connect(osso.ic.IAP_ANY, osso.ic.IAP_REQUESTED_CONNECTION)
+        print "_bt_connect_cb"
+        print osso.ic.connect(osso.ic.IAP_ANY, osso.ic.REQUESTED_CONNECT)
+        self.status.set_text("Connecting...")
 
     def _bt_disconnect_cb(self, bt):
-        pass
+        print "_bt_disconnect_cb"
+        print osso.ic.disconnect(osso.ic.IAP_ANY)
+        self.status.set_text("Disconnecting...")
 
     def _update_stats(self, evt):
-        pass
+        print "_update_stats"
+
+        print evt.time_active
+        print evt.signal_strength
+        print evt.rx_packets
+        print evt.tx_packets
+        print evt.rx_bytes
+        print evt.tx_bytes
+
+        self.time.set_text(str(evt.time_active))
+        self.signal.set_text(str(evt.signal_strength))
+        self.rx_packets.set_text(str(evt.rx_packets))
+        self.tx_packets.set_text(str(evt.tx_packets))
+        self.tx_bytes.set_text(str(evt.tx_bytes))
+        self.rx_bytes.set_text(str(evt.rx_bytes))
+
+    def _req_stats(self):
+        if self.status.get_text() == "Connected": # Ugly...
+            osso.ic.get_statistics()
+        else:
+            return False
+
+        return True
 
 if __name__ == "__main__":
     main = ExampleIc()
